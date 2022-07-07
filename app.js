a56922dfa22366ac4879da18ee5ffbf8cec3ee7b
miraclepbspbs <304088570@qq.com>
2022-07-07 21:45:58 +0800

    update

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const _ = require("lodash");
-
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("home page");
+  } else if (req.url === "/about") {
+    res.end("about page");
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${i}, ${j}`);
+      }
+    }
+  } else {
+    res.end("erro");
+  }
+});
+server.listen(5000, () => {
+  console.log("he is listening 5000,...");
+});
